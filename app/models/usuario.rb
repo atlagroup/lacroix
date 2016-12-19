57e2da8a9c6b9915d408f905a75c7ad5bd8a1b84
Henrique Sousa <rickftime@gmail.com>
2016-12-19 17:37:32 -0200

    Criado model Usuario

---
+++ app/models/usuario.rb
@@ -0,0 +1,9 @@
+class Usuario < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Password pode ser acessado pelo nome senha
+  alias_attribute :senha, :password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :usuarios, path: '', 
+    path_names: { sign_in: :entrar, sign_out: :sair, sign_up: :cadastrar },
+    controllers: {
+      sessions: 'usuarios/sessions',
+      registrations: 'usuarios/registrations'
+    }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "main#index"
 
